8ee269ac6ca7419992e84345df10b622cd8e19a1
Kriskros Kriskros <kriskros341>
2022-12-03 11:13:40 +0100

    kargioejg

--- front-end/src/Components/Calendar.tsx
+++ front-end/src/Components/Calendar.tsx
@@ -64,7 +64,6 @@ const Calendar: React.FC<CalendarComponentInterface> = ({editTask}) => {
     const proj = user.data?.worker.projects || []
     const taskdata = proj.map(v => v.tasks).flat()
 
-
     const weekData = new Array<HourData[]>(7)
     for(let i = 0; i < 7; i++) {
         weekData[i] = []
@@ -73,14 +72,15 @@ const Calendar: React.FC<CalendarComponentInterface> = ({editTask}) => {
 
         }
     }
+    console.log("jd", proj)
     for (let task of taskdata) {
         const date = new Date(parseInt(task.date))
-        const dayOfTheWeek = date.getDay()
+        const dayOfTheWeek = date.getDay()-1
         const start = date.getHours()
+        console.log(task)
         for (let hour = start; hour < start + task.shouldTake; hour++) {
             weekData[dayOfTheWeek][hour].task = task.type
             weekData[dayOfTheWeek][hour].change = () => editTask(task.id)
-            
         }
     }
     return (
--- front-end/src/Components/CustomizeTask.tsx
+++ front-end/src/Components/CustomizeTask.tsx
@@ -23,6 +23,7 @@ const CustomizeTask: React.FC<CustomizeTaskInterface> = ({taskId}) => {
     const taskData = useQuery<{task: Task}>(taskQuery1, {variables: {taskId: taskId}})
     const data = taskData.data?.task
     console.log(taskId, taskData)
+    console.log(data?.date)
     const [ dates, setDates ] = useState(data?.date ? new Date(parseInt(data.date)) : new Date())
     const input1Ref = useRef<HTMLInputElement>(null)
     const input2Ref = useRef<HTMLInputElement>(null)
@@ -33,14 +34,23 @@ const CustomizeTask: React.FC<CustomizeTaskInterface> = ({taskId}) => {
     }
     const {type, shouldTake, date} = data
     const startHour = new Date().getHours()
-    const usequery = () => {
-
-    }
     const sendMutation = () => {
         const d = new Date(dates)
-        console.log(d)
-        d.setHours(startHour)
-        mutationFunction({variables: {mutTaskId: taskId, task: {type: input1Ref.current?.value || "", date: d.getTime().toString()}}})
+        const newHour = input2Ref.current?.value
+        newHour && d.setHours(parseInt(newHour))
+        const timeShouldTake = input3Ref.current?.value
+
+         
+
+        mutationFunction({variables: {
+            mutTaskId: taskId, 
+            task: {
+                type: input1Ref.current?.value || "", 
+                date: d.getTime().toString(),
+                shouldTake: timeShouldTake ? parseInt(timeShouldTake) : shouldTake
+            }}}
+                
+        )
     }
     return (
         <div onClick={(e) => {e.stopPropagation()}} className={"CustomizeTaskContainer"} >
